00cd2c17ff2d33dfeff1db5805b59165c34ea03f
Nightqq <465646039@qq.com>
2017-10-31 15:21:51 +0800

    saff

--- app/src/main/java/com/zxdz/car/main/model/domain/ChangePolice.java
+++ app/src/main/java/com/zxdz/car/main/model/domain/ChangePolice.java
@@ -1,9 +1,15 @@
 package com.zxdz.car.main.model.domain;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+
 /**
  * Created by Lenovo on 2017/10/31.
  */
-
+@Entity
 public class ChangePolice {
-
+    @Id
+    private int change_id;
+    private String police_num;
+    private String driver_num;
 }
